1a4b68ac38dc77285c21b322d66a02f0a1e374b9
ARVIND-36 <pnarvind06@gmail.com>
2026-01-23 11:58:10 +0530

    FEW CHANGES

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use 0.0.0.0 to allow external connections (required for Docker)
+    host = os.environ.get("FLASK_HOST", "127.0.0.1")
+    port = int(os.environ.get("FLASK_PORT", 5000))
+    debug = os.environ.get("FLASK_DEBUG", "true").lower() == "true"
+    app.run(host=host, port=port, debug=debug)
